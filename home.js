5f3d390f4a8a31b904ed4cfb70aad6cff596addb
Haardispro <62053026+Haardispro@users.noreply.github.com>
2021-05-26 12:37:58 +0530

    done

--- home.js
+++ home.js
@@ -1,5 +1,12 @@
 console.log("hello world");
-
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 
 /*
 function hide() {
